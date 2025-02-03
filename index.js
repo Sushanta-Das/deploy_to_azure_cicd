010b1ff8b8ee78ea61e96071060c6cc18226f7d1
Sushanta Das <susantadas59713@gmail.com>
2025-02-03 18:47:07 +0530

    source code is pushed

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express")
+
+const app=express()
+
+
+
+const port= process.env.PORT || 8000
+
+
+app.get('/', (req, res) => {
+    res.status(200).send("this is deployment from github actions to azure")
+    
+  })
+
+  app.listen(port,()=>
+  {
+    console.log("app is running")
+  })
